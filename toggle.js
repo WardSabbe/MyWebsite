8f241cf41c8bb07f48672615afcc74d518810434
Ward Sabbe <ward.sabbe@ugent.be>
2023-05-15 00:03:04 +0200

    light and dark mode done

--- toggle.js
+++ toggle.js
@@ -1,21 +1,32 @@
 
-    function toggleMode() {
-      var element = document.body;
-      var container = document.getElementById("image-container");
-      var darkImage = document.getElementById("dark-image");
-      var lightImage = document.getElementById("light-image");
-  
-      if (element.classList.contains("dark-mode")) {
-        element.classList.remove("dark-mode");
-        element.classList.add("light-mode");
-        darkImage.style.display = "none";
-        lightImage.style.display = "block";
-      } else {
-        element.classList.remove("light-mode");
-        element.classList.add("dark-mode");
-        lightImage.style.display = "none";
-        darkImage.style.display = "block";
-      }
-    }
-  
-    toggleMode();
+
+function toggleMode() {
+  var element = document.body;
+  var container = document.getElementById("image-container");
+  var darkImage = document.getElementById("dark-image");
+  var lightImage = document.getElementById("light-image");
+
+  if (element.classList.contains("dark-mode")) {
+    element.classList.remove("dark-mode");
+    element.classList.add("light-mode");
+    darkImage.style.display = "none";
+    lightImage.style.display = "block";
+    localStorage.setItem("mode", "light"); // sla de keuze van de gebruiker op
+  } else {
+    element.classList.remove("light-mode");
+    element.classList.add("dark-mode");
+    lightImage.style.display = "none";
+    darkImage.style.display = "block";
+    localStorage.setItem("mode", "dark"); // sla de keuze van de gebruiker op
+  }
+}
+      
+var mode = localStorage.getItem("mode");
+if (mode === "dark") {
+  document.body.classList.add("dark-mode");
+  document.getElementById("dark-image").style.display = "block";
+} else {
+  document.body.classList.add("light-mode");
+  document.getElementById("light-image").style.display = "block";
+}
+
